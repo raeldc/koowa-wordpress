634cbd851da728d3169521a2ebe6026752c91ccf
Israel D. Canasa <raeldc@gmail.com>
2014-03-02 01:56:48 +0800

    Added the html tag filter abstract. Also added adminmenu and submenu filters.
    This html tag filter will look for tags and its attributes so they can be manipulated on render.

---
+++ components/koowa/template/filter/submenu.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Koowa for Wordpress
+ *
+ * @copyright   Copyright (C) 2014 Israel D. Canasa and WIZMEDIA (http://wizmediateam.com)
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        git://git.assembla.com/nooku-framework.git for the canonical source repository
+ */
+
+/**
+ * Submenu Template Filter
+ *
+ * @author  Israel Canasa <http://github.com/raeldc>
+ * @package Wordpress\Template\Filter
+ */
+class ComKoowaTemplateFilterSubmenu extends ComKoowaTemplateFilterTag
+{
+    protected function _initialize(KObjectConfig $config)
+    {
+        $config->append(array(
+            'tag' => 'submenu'
+        ));
+    }
+    /**
+     * Render the tag
+     *
+     * @param   array   $attribs Associative array of attributes
+     * @param   string  $content The tag content
+     * @return string
+     */
+    public function render(&$text)
+    {
+        parent::render($text);
+
+        foreach ($this->_parsed_tags as $tag)
+        {
+            $tag->append(array(
+                'page_title' => $tag->content,
+                'capability' => 'manage_options',
+                'menu_slug'  => $this->getIdentifier()->package,
+                'icon_url' => '',
+                'position' => null
+            ));
+
+            add_submenu_page(
+                $tag->page_title,
+                $tag->content, 
+                $tag->capability,
+                $tag->menu_slug,
+                array($this->getObject('application'), 'route'),
+                $tag->icon_url,
+                $tag->position
+            );
+        }
+    }
+}
---
+++ components/koowa/template/filter/tag.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Koowa for Wordpress
+ *
+ * @copyright   Copyright (C) 2014 Israel D. Canasa and WIZMEDIA (http://wizmediateam.com)
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        git://git.assembla.com/nooku-framework.git for the canonical source repository
+ */
+
+/**
+ * Tag Template Filter
+ *
+ * @author  Israel Canasa <http://github.com/raeldc>
+ * @package Wordpress\Template\Filter
+ */
+abstract class ComKoowaTemplateFilterTag extends KTemplateFilterAbstract
+{
+    protected $_tag;
+    protected $_parsed_tags = array();
+
+    /**
+     * Constructor.
+     *
+     * @param   KObjectConfig $config Configuration options
+     */
+    public function __construct(KObjectConfig $config)
+    {
+        parent::__construct($config);
+
+        $this->_priority = $config->priority;
+        $this->_tag = $config->tag;
+    }
+
+    /**
+     * Initializes the options for the object
+     *
+     * Called from {@link __construct()} as a first step of object instantiation.
+     *
+     * @param   KObjectConfig $config Configuration options
+     * @return  void
+     */
+    protected function _initialize(KObjectConfig $config)
+    {
+        $config->append(array(
+            'priority' => self::PRIORITY_LOW,
+            'tag' => ''
+        ));
+
+        parent::_initialize($config);
+    }
+
+    /**
+     * Find any virtual tags and render them
+     *
+     * This function will pre-pend the tags to the content
+     *
+     * @param string $text  The text to parse
+     */
+    public function render(&$text)
+    {
+        //Parse the tags
+        $this->_parseTags($text);
+    }
+
+    /**
+     * Parse the text for html tags
+     *
+     * @param string $text  The text to parse
+     * @return array
+     */
+    protected function _parseTags(&$text)
+    {
+        if (empty($this->_tag)) return;
+
+        $matches = array();
+        if(preg_match_all('#<'.$this->_tag.'(.*)>(.*)<\/'.$this->_tag.'>#siU', $text, $matches))
+        {
+            foreach(array_unique($matches[2]) as $key => $match)
+            {
+                $attribs = array_merge(array('content' => $match), $this->parseAttributes($matches[1][$key]));
+                $this->_parsed_tags[] = new KObjectConfig($attribs);
+            }
+
+            $text = str_replace($matches[0], '', $text);
+        }
+    }
+}
