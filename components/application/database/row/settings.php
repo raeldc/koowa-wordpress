b651da5e8fe5e453eb6cb7f377da985334b85c43
Israel D. Canasa <raeldc@gmail.com>
2014-03-14 10:11:09 +0800

    Saving of pages is now separated into a function

--- components/application/database/row/settings.php
+++ components/application/database/row/settings.php
@@ -8,53 +8,8 @@ class ComApplicationDatabaseRowSettings extends KDatabaseRowTable
         $settings  = $this->getObject('lib:object.config.factory')->getFormat('ini');
         $settings->add(array_merge((array)$this->settings, array('last_modified' => date( 'Y-m-d H:i:s'))));
 
-        $pages          = $this->getObject('com:application.model.pages');
-        $new_pages      = $this->pages;
-        $assigned_pages = array();
-        $updated_pages  = array();
-
-        // Get the pages that were assigned
-        foreach ($new_pages as $page)
-        {
-            if(empty($page['id'])) {
-                continue;
-            }
-
-            $page['component']           = $component;
-            $assigned_pages[$page['id']] = $page;
-        }
-
-        // Query all existing pages with the assigned pages
-        $pages->getState()->id = array_keys($assigned_pages);
-        $existing_pages = $pages->getList();
-
-        // Update the existing pages
-        if ($existing_pages->count()) 
-        {
-            foreach ($existing_pages as $page)
-            {
-                $page->setData($assigned_pages[$page->id])->save();
-                $updated_pages[] = $page->id;
-                unset($assigned_pages[$page->id]);
-            }
-        }
-
-        // Create non-existing page
-        foreach ($assigned_pages as $page)
-        {
-            $pages->getTable()->getRow()->setData($page)->save();
-            $updated_pages[] = $page['id'];
-            unset($assigned_pages[$page['id']]);
-        }
-
-        // Delete others that are not assigned
-        $pages->reset()->getState()->setValues(array(
-            'component' => $component,
-            'exclude' => $updated_pages
-        ));
-
-        foreach ($pages->getList() as $page) {
-            $page->delete();
+        if ($this->pages) {
+            $this->savePages($this->pages);
         }
 
         $this->reset();
@@ -66,6 +21,58 @@ class ComApplicationDatabaseRowSettings extends KDatabaseRowTable
         $this->settings = $settings;
 
         return parent::save();
+    }
+
+    public function savePages(array $pages = array())
+    {
+        $pagesmodel     = $this->getObject('com:application.model.pages');
+        $assigned_pages = array();
+        $updated_pages  = array();
+
+        // Get the pages that were assigned
+        foreach ($pages as $page)
+        {
+            if(empty($page['id'])) {
+                continue;
+            }
+
+            $page['component']           = $component;
+            $assigned_pages[$page['id']] = $page;
+        }
+
+        // Query all existing pages with the assigned pages
+        $pagesmodel->getState()->id = array_keys($assigned_pages);
+
+        // Update the existing pages
+        if ($assigned_pages && $pagesmodel->getTotal()) 
+        {
+            foreach ($pagesmodel->getList() as $page)
+            {
+                $page->setData($assigned_pages[$page->id])->save();
+                $updated_pages[] = $page->id;
+                unset($assigned_pages[$page->id]);
+            }
+        }
+
+        // Create non-existing page
+        foreach ($assigned_pages as $page)
+        {
+            $pagesmodel->getTable()->getRow()->setData($page)->save();
+            $updated_pages[] = $page['id'];
+            unset($assigned_pages[$page['id']]);
+        }
+
+        // Delete others that are not assigned
+        $pagesmodel->reset()->getState()->setValues(array(
+            'component' => $component,
+            'exclude' => $updated_pages
+        ));
+
+        foreach ($pagesmodel->getList() as $page) {
+            $page->delete();
+        }
+
+        return $this;
     }
 
     public function __get($key)
