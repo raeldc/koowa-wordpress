8394770720e26a4c1459bfe84547166a3a582972
Israel D. Canasa <raeldc@gmail.com>
2014-04-09 10:46:53 +0800

    Moved the unminified checkbox-button.js to /javascript

--- media/javascript/checkbox-button.js
+++ media/javascript/checkbox-button.js
@@ -1,6 +1,40 @@
 (function($){
-    'use strict';
-    $('[data-toggle="checkbox"]').find('.btn').click(function(){
-        console.log('hello');
+    $(document).ready(function(){
+        $('[data-toggle="checkbox"]').find('.btn').click(function(e){
+            e.preventDefault();
+            var $this     = $(this);
+            var $parent   = $this.closest('[data-toggle="checkbox"]');
+            var $input    = $parent.find('input');
+            var $inputbtn = $input.closest('.btn');
+            var selected  = false;
+
+            // If this is not the button that hold the input button
+            if (this !== $inputbtn[0]) {
+                selected = $this.data('selected') !== 'false' && $this.data('selected') !== false && $this.hasClass('active');
+                // Do nothing
+                if (selected) {
+                    return false;
+                }
+                // Select this button
+                $this.addClass('active');
+                $this.data('selected', true);
+                // Unselect the others
+                $input.prop('checked', false);
+                $inputbtn.removeClass('active');
+            }else{
+                selected = $input.prop('checked') && $this.hasClass('active');
+                // Do nothing
+                if (selected) {
+                    return false;
+                }
+                // Unselect the others
+                $parent.find('.btn').removeClass('active');
+                // Select this button
+                $this.addClass('active');
+                $input.prop('checked', true);
+            }
+
+            return false;
+        });
     });
 })(jQuery);
